ed472932eb867063b813355a1e4b8b9ae9c65c77
Bruno Rodrigues <brunoo00@motorola.com>
2022-01-27 14:28:55 -0300

    Adding material to the project

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -5,11 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent implements OnInit {
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'NO CONTENT';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onAddPost() {
+    this.newPost = this.enteredValue;
   }
 
 }
